208d8a2986c02297afb3f862698e0dc34c4b9cac
Maksymyr <maksymyr@gmail.com>
2017-10-17 01:18:49 +0300

    Three in row beta 1.2

--- GoIT/Week5/Three-in-row/js/js.js
+++ GoIT/Week5/Three-in-row/js/js.js
@@ -1,7 +1,7 @@
-	// var color=['images/paladin.png','images/rogue.png','images/mage.png','images/shaman.png','images/warrior.png','images/druid.png'];
+	// var color=['images/mage.svg','images/rogue.svg','images/hunter.svg','images/shaman.svg','images/warrior.svg','images/druid.svg'];
 	var color=['red','blue','green','yellow','orange','violet'];
 	
-	var side = 7;
+	var side = +prompt("Three in row game! Enter the size of board please:", 7);
 	var restartFlag = 0;
 	var checkbox = true;
 	var score = 0;
@@ -15,8 +15,6 @@
 	var start = document.getElementById('start');
 	
 		start.onclick = StartGame;
-		
-	
 	
 	function StartGame() {
 		if (checkbox) {
@@ -29,9 +27,9 @@
 				restart.onclick = StartGame;
 			}
 			if (restartFlag > 0) {
-				for (var i = 0; i < side * side; i++) {
 					wrap.innerHTML = '';
-				}
+					score = 0;
+				score_box.value = score;
 			}
 			wrap = document.getElementById('wrap');
 			wrap.style.width = side * 50 + 'px';
@@ -63,47 +61,67 @@
 				var click = 0;
 				var jewFirst= 0;
 				var allJews = wrap.getElementsByClassName('jewels');
+				CheckThree();
 				for (var i = 0; i < allJews.length; i++) {
 					allJews[i].onclick = JewClick;
 				}
 				function JewClick() {
-	
-					
-					
-					
 					if (click === 0) {
 						jewFirst = event.target;
 						event.target.style.border = '2px solid white';
 						click++;
 					}
 					else if (click === 1) {
+						var jewSecond = event.target;
 						var data = parseInt(jewFirst.getAttribute("data-n"));
-						var data2 = parseInt(event.target.getAttribute("data-n"));
-						if (data+7 === data2 || data-7 === data2 || data+1 === data2 || data-1 === data2) {
-							var firstColor = jewFirst.getAttribute('type');
-							jewFirst.setAttribute('type', event.target.getAttribute('type'));
-							jewFirst.style.backgroundColor = color[event.target.getAttribute('type')];
-							jewFirst.style.border = 'none';
-							var secondColor = event.target.getAttribute('type');
-							// event.target.removeAttribute('type');
-							event.target.setAttribute('type', firstColor);
-							event.target.style.backgroundColor = color[firstColor];
-							if (!CheckThree()) {
-								jewFirst.setAttribute('type', firstColor);
-								jewFirst.style.backgroundColor = color[firstColor];
-								jewFirst.style.border = 'none';
-								event.target.setAttribute('type', secondColor);
-								event.target.style.backgroundColor = color[secondColor];
+						var data2 = parseInt(jewSecond.getAttribute("data-n"));
+						if (data+side === data2 || data-side === data2 || data+1 === data2 || data-1 === data2) {
+							if (data+side === data2) {
+								jewSecond.classList.add("anim-class-down");
+								jewFirst.classList.add("anim-class-up");
 							}
-							click = 0;
-							jewFirst = 0;
-							
+							else if (data-side === data2) {
+								jewSecond.classList.add("anim-class-up");
+								jewFirst.classList.add("anim-class-down");
+							}
+							else if (data+1 === data2) {
+								jewSecond.classList.add("anim-class-left");
+								jewFirst.classList.add("anim-class-right");
+							}
+							else if (data-1 === data2) {
+								jewSecond.classList.add("anim-class-right");
+								jewFirst.classList.add("anim-class-left");
+							}
+							jewFirst.style.border = 'none';
+							setTimeout(function ()
+							{
+								jewSecond.classList.remove("anim-class-right","anim-class-left","anim-class-down","anim-class-up");
+								jewFirst.classList.remove("anim-class-right","anim-class-left","anim-class-down","anim-class-up");
+	
+								var firstColor = jewFirst.getAttribute('type');
+								jewFirst.setAttribute('type', jewSecond.getAttribute('type'));
+								jewFirst.style.backgroundColor = color[jewSecond.getAttribute('type')];
+								
+								var secondColor = jewSecond.getAttribute('type');
+								jewSecond.setAttribute('type', firstColor);
+								jewSecond.style.backgroundColor = color[firstColor];
+								if (!CheckThree()) {
+									jewFirst.setAttribute('type', firstColor);
+									jewFirst.style.backgroundColor = color[firstColor];
+									jewFirst.style.border = 'none';
+									jewSecond.setAttribute('type', secondColor);
+									jewSecond.style.backgroundColor = color[secondColor];
+								}
+								click = 0;
+								jewFirst = 0;
+							}, 1000);
 						}
 						else {
 							jewFirst.style.border = 'none';
 							click = 0;
 							jewFirst = 0;
 						}
+				
 					}
 				}
 				
@@ -204,20 +222,20 @@
 		return color.indexOf(newType);
 	}
 	function CheckThree() {
-		for (var j = 1; j < 8; j++) {
+		for (var j = 1; j < side+1; j++) {
 			var type_check_row = null;
 			var count_check_row = 0;
 			var array_check_row = [];
 			var type_check_col = null;
 			var count_check_col = 0;
 			var array_check_col = [];
-			for (var i = 0; i < 6; i++) {
+			for (var i = 0; i < side-1; i++) {
 				if (document.getElementsByClassName("row"+j)[i].firstChild.getAttribute("type") ===
 					document.getElementsByClassName("row"+j)[i+1].firstChild.getAttribute("type")) {
 					count_check_row++;
 					array_check_row.push(i);
 					type_check_row = document.getElementsByClassName("row"+j)[i].firstChild.getAttribute("type");
-					if (i===5) {
+					if (i===side-2) {
 						if (count_check_row >= 2) {
 							DestroyThreeRow(count_check_row+1, array_check_row, j);
 							ScoreCount(count_check_row+1);
@@ -249,7 +267,7 @@
 					count_check_col++;
 					array_check_col.push(i);
 					type_check_col = document.getElementsByClassName("col"+j)[i].firstChild.getAttribute("type");
-					if (i===5) {
+					if (i===side-2) {
 						if (count_check_col >= 2) {
 							DestroyThreeCol(count_check_col+1, array_check_col, j);
 							ScoreCount(count_check_col+1);
@@ -286,12 +304,14 @@
 				var type_temp = document.getElementsByClassName("row"+(j))[index_st+i].firstChild.getAttribute("type");
 				document.getElementsByClassName("row"+(j+1))[index_st+i].firstChild.setAttribute("type", type_temp);
 				document.getElementsByClassName("row"+(j+1))[index_st+i].firstChild.style.backgroundColor = color[type_temp];
+				// document.getElementsByClassName("row"+(j+1))[index_st+i].firstChild.style.backgroundImage='url("' + color[type_temp] + '")';
 			}
 			var newTypeRand = Math.floor(Math.random()*6);
-			document.getElementsByClassName("row1")[index_st+i].firstChild.setAttribute("type", newTypeRand);
-			document.getElementsByClassName("row1")[index_st+i].firstChild.style.backgroundColor = color[newTypeRand];
+				document.getElementsByClassName("row1")[index_st + i].firstChild.setAttribute("type", newTypeRand);
+				document.getElementsByClassName("row1")[index_st + i].firstChild.style.backgroundColor = color[newTypeRand];
+			// document.getElementsByClassName("row1")[index_st + i].firstChild.style.backgroundImage='url("' + color[newTypeRand] + '")';
 		}
-		setTimeout(CheckThree(),1000);
+		setTimeout(function() {CheckThree()},800);
 	}
 	function DestroyThreeCol(count, array, col) {
 		var index_st = array[1]+2;
@@ -300,15 +320,17 @@
 						var type_temp = document.getElementsByClassName("row" + (j - count))[col - 1].firstChild.getAttribute("type");
 						document.getElementsByClassName("row" + (j))[col - 1].firstChild.setAttribute("type", type_temp);
 						document.getElementsByClassName("row" + (j))[col - 1].firstChild.style.backgroundColor = color[type_temp];
+						// document.getElementsByClassName("row" + (j))[col - 1].firstChild.style.backgroundImage='url("' + color[type_temp] + '")';
 					}
 					else {
 						var newTypeRand = Math.floor(Math.random()*6);
 						document.getElementsByClassName("row"+j)[col-1].firstChild.setAttribute("type", newTypeRand);
 						document.getElementsByClassName("row"+j)[col-1].firstChild.style.backgroundColor = color[newTypeRand];
+						// document.getElementsByClassName("row"+j)[col-1].firstChild.style.backgroundImage='url("' + color[newTypeRand] + '")';
 					}
 				}
 		
-		setTimeout(CheckThree(),1000);
+		setTimeout(function() {CheckThree()},800);
 	}
 	function ScoreCount(count) {
 		switch (count) {
