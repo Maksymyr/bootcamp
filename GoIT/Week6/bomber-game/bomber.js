b99736aeccfd7916ba82596661716ba6857cccc1
Maksymyr <maksymyr@gmail.com>
2017-10-19 23:51:37 +0300

    bomber alfa 2.1

--- GoIT/Week6/bomber-game/bomber.js
+++ GoIT/Week6/bomber-game/bomber.js
@@ -43,84 +43,107 @@ $(function(){
 
 	});
 	//----move
-	function move(e){
-		var e=e||event;
+	function move(ev){
+		var e=ev||event;
 		var k=e.keyCode;
-		// console.log(k);
+
 		/*PLAYER 1 moves-----------*/
-		if(k==39){	
+		if(k==39){
+			var posright = $(".item").eq(pos+1);
+			if((pos+1)%side===0 || $(posright).hasClass("brick-wall") || $(posright).hasClass("steel-wall")
+				|| $(posright).hasClass("hero2") || $(posright).hasClass("bomb")) {
+			}
+			else {
 			$('.item').eq(pos).removeClass('hero');
 			pos=pos+1;
-			if(pos%side==0){pos=pos-1}
-			if ($(".item").eq(pos).hasClass("brick-wall")){pos=pos-1}
-			if ($(".item").eq(pos).hasClass("steel-wall")){pos=pos-1}
-				$('.item').eq(pos).addClass('hero');
-			boom();
+			$('.item').eq(pos).addClass('hero');
+			boom();}
 		}
 		if(k==37){
+			var posleft = $(".item").eq(pos-1);
+			if((pos)%side===0 || $(posleft).hasClass("brick-wall") || $(posleft).hasClass("steel-wall")
+				|| $(posleft).hasClass("hero2") || $(posleft).hasClass("bomb")) {
+			}
+			else {
 			$('.item').eq(pos).removeClass('hero');
 			pos=pos-1;
-			if((pos+1)%side==0){pos=pos+1}
-			if ($(".item").eq(pos).hasClass("brick-wall")){pos=pos+1}
-			if ($(".item").eq(pos).hasClass("steel-wall")){pos=pos+1}
 				$('.item').eq(pos).addClass('hero');
-			
+
 			boom();
 		}
-		if(k==38){
-			$('.item').eq(pos).removeClass('hero');
-			pos=pos-side;
-			if(pos<0){pos=pos+side}
-			if ($(".item").eq(pos).hasClass("brick-wall")){pos=pos+side}
-			if ($(".item").eq(pos).hasClass("steel-wall")){pos=pos+side}
+		}
+		if(k==38) {
+			var postop = $(".item").eq(pos - side);
+			if (pos-side < 0 || $(postop).hasClass("brick-wall") || $(postop).hasClass("steel-wall")
+				|| $(postop).hasClass("hero2") || $(postop).hasClass("bomb")) {
+			}
+			else {
+				$('.item').eq(pos).removeClass('hero');
+				pos = pos - side;
 				$('.item').eq(pos).addClass('hero');
-			boom();
+				boom();
+			}
 		}
 		if(k==40){
-			$('.item').eq(pos).removeClass('hero');
-			pos=pos+side;
-			if(pos>=side*side){pos=pos-side}
-			if ($(".item").eq(pos).hasClass("brick-wall")){pos=pos-side}
-			if ($(".item").eq(pos).hasClass("steel-wall")){pos=pos-side}
+			var posdown = $(".item").eq(pos+side);
+			if(pos+side>=side*side || $(posdown).hasClass("brick-wall") || $(posdown).hasClass("steel-wall")
+				|| $(posdown).hasClass("hero2") || $(posdown).hasClass("bomb")) {
+			}
+			else {
+				$('.item').eq(pos).removeClass('hero');
+				pos=pos+side;
 				$('.item').eq(pos).addClass('hero');
-			boom();
+				boom();
+			}
 		}
 		/*PLAYER 2 moves-----------*/
-		if(k==68){	
-			$('.item').eq(pos2).removeClass('hero2');
-			pos2=pos2+1;
-			if(pos2%side==0){pos2=pos2-1}
-			if ($(".item").eq(pos2).hasClass("brick-wall")){pos2=pos2-1}
-			if ($(".item").eq(pos2).hasClass("steel-wall")){pos2=pos2-1}
+		if(k==68) {
+			var pos2right = $(".item").eq(pos2 + 1);
+			if ((pos2 + 1) % side === 0 || $(pos2right).hasClass("brick-wall") || $(pos2right).hasClass("steel-wall")
+				|| $(pos2right).hasClass("hero") || $(pos2right).hasClass("bomb")) {
+			}
+			else {
+				$('.item').eq(pos2).removeClass('hero2');
+				pos2 = pos2 + 1;
 				$('.item').eq(pos2).addClass('hero2');
-			boom();
+				boom();
+			}
 		}
-		if(k==65){
-			$('.item').eq(pos2).removeClass('hero2');
-			pos2=pos2-1;
-			if((pos2+1)%side==0){pos2=pos2+1}
-			if ($(".item").eq(pos2).hasClass("brick-wall")){pos2=pos2+1}
-			if ($(".item").eq(pos2).hasClass("steel-wall")){pos2=pos2+1}
+		if(k==65) {
+			var pos2left = $(".item").eq(pos2 - 1);
+			if (pos2 % side === 0 || $(pos2left).hasClass("brick-wall") || $(pos2left).hasClass("steel-wall")
+				|| $(pos2left).hasClass("hero") || $(pos2left).hasClass("bomb")) {
+			}
+			else {
+				$('.item').eq(pos2).removeClass('hero2');
+				pos2 = pos2 - 1;
 				$('.item').eq(pos2).addClass('hero2');
-			boom();
+				boom();
+			}
 		}
-		if(k==87){
-			$('.item').eq(pos2).removeClass('hero2');
-			pos2=pos2-side;
-			if(pos2<0){pos2=pos2+side}
-			if ($(".item").eq(pos2).hasClass("brick-wall")){pos2=pos2+side}
-			if ($(".item").eq(pos2).hasClass("steel-wall")){pos2=pos2+side}
+		if(k==87) {
+			var pos2top = $(".item").eq(pos2 - side);
+			if ((pos2 - side) < 0 || $(pos2top).hasClass("brick-wall") || $(pos2top).hasClass("steel-wall")
+				|| $(pos2top).hasClass("hero") || $(pos2top).hasClass("bomb")) {
+			}
+			else {
+				$('.item').eq(pos2).removeClass('hero2');
+				pos2 = pos2 - side;
 				$('.item').eq(pos2).addClass('hero2');
-			boom();
+				boom();
+			}
 		}
-		if(k==83){
-			$('.item').eq(pos2).removeClass('hero2');
-			pos2=pos2+side;
-			if(pos2>=side*side){pos2=pos2-side}
-			if ($(".item").eq(pos2).hasClass("brick-wall")){pos2=pos2-side}
-			if ($(".item").eq(pos2).hasClass("steel-wall")){pos2=pos2-side}
+		if(k==83) {
+			var pos2down = $(".item").eq(pos2 + side);
+			if ((pos2 + side) >= side * side || $(pos2down).hasClass("brick-wall") || $(pos2down).hasClass("steel-wall")
+				|| $(pos2down).hasClass("hero") || $(pos2down).hasClass("bomb")) {
+			}
+			else {
+				$('.item').eq(pos2).removeClass('hero2');
+				pos2 = pos2 + side;
 				$('.item').eq(pos2).addClass('hero2');
-			boom();
+				boom();
+			}
 		}
 		if(k==13){
 			setBomb(pos);
@@ -144,7 +167,7 @@ $(function(){
 			$(".item").eq(bombpos).removeClass('bomb');
 			$(".item").eq(bombpos).addClass('explosion');
 
-			var explotionLength = 5;
+			var explotionLength = 1;
 			var rCh = true, tCh = true, dCh = true, lCh = true;
 			for (var i = 1; i <= explotionLength; i++) {
 				if ((bombpos+i)%side < bombpos%side) {
@@ -203,31 +226,19 @@ $(function(){
 						}
 					}
 				}
-				
 			}
-			
 			boom();
 			setTimeout(function removeExp(){
-				for (var i = 1; i <= explotionLength; i++) {
-					$(".item").eq(bombpos).removeClass('explosion');
-					$(".item").eq(bombpos + i).removeClass('explosion');
-					$(".item").eq(bombpos - i).removeClass('explosion');
-					$(".item").eq(bombpos + side*i).removeClass('explosion');
-					$(".item").eq(bombpos - side*i).removeClass('explosion');
+				var limit = $(".explosion").length;
+				for (var i = 1; i <= limit; i++) {
+					$(".explosion").eq(-1).removeClass('explosion');
 				}
 			}, 250)
 		}, 2000)
-
 	}
-	
-
 	function boom(){
 		if ($('.explosion').hasClass('hero')) {setTimeout("alert('Hero1 Die')", 200) }
 		if ($('.explosion').hasClass('hero2')) {setTimeout("alert('Hero2 Die')", 200)}
 		if ($('.explosion').hasClass('brick-wall')){$('.explosion.brick-wall').removeClass('brick-wall')}
-
-
 		}
-
-
 });
