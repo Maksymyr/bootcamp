171ed255282d5422e8366e6e21850088ccf9ba23
Maksymyr <maksymyr@gmail.com>
2017-10-20 17:48:07 +0300

    snow2

--- GoIT/Week6/W6D4/Snow/js.js
+++ GoIT/Week6/W6D4/Snow/js.js
@@ -47,8 +47,23 @@ class Snowball {
 	moving() {
 		this.draw();
 		this.y += this.speed;
-		if (this.y >= h+this.radius) {
+		if (this.y > h) {
 			this.y = 0;
+			this.x = Math.floor(Math.random() * w);
+			this.speed = Math.floor(Math.random() * 3+2);
+		}
+		if (this.x > w){
+			this.x = 0;
+			this.y = Math.floor(Math.random() * w);
+			this.speed = Math.floor(Math.random() * 3+2);
+		}
+		if (this.x < 0){
+			this.x = w;
+			this.y = Math.floor(Math.random() * w);
+			this.speed = Math.floor(Math.random() * 3+2);
+		}
+		if (this.y < 0) {
+			this.y = h;
 			this.x = Math.floor(Math.random() * w);
 			this.speed = Math.floor(Math.random() * 3+2);
 		}
@@ -59,9 +74,43 @@ var snowArray = [];
 for (var n = 0; n < 100; n++) {
 	setTimeout(function () {
 		snowArray.push(new Snowball(Math.floor(Math.random() * w), Math.floor(Math.random() * 3 + 2)));
+		// console.log(snowArray);
 	}, 100*n);
 }
 
 
 ctx.fillStyle = "white";
 window.requestAnimationFrame(drawIt);
+
+
+var lastX = 0;
+var lastY = 0;
+document.onmousemove = function (e) {
+	console.log(e.pageX + " : " + e.pageY);
+	if (e.pageX <= lastX && e.pageY <= lastY) {
+		for (var i = 0; i < snowArray.length; i++) {
+			snowArray[i].x = snowArray[i].x+(e.pageX - lastX);
+			snowArray[i].y = snowArray[i].y+(e.pageY - lastY);
+		}
+	}
+	else if (e.pageX >= lastX && e.pageY <= lastY) {
+		for (var i = 0; i < snowArray.length; i++) {
+			snowArray[i].x = snowArray[i].x-(lastX - e.pageX);
+			snowArray[i].y = snowArray[i].y+(e.pageY - lastY);
+		}
+	}
+	else if (e.pageX >= lastX && e.pageY >= lastY) {
+		for (var i = 0; i < snowArray.length; i++) {
+			snowArray[i].x = snowArray[i].x-(lastX - e.pageX);
+			snowArray[i].y = snowArray[i].y-(lastY - e.pageY);
+		}
+	}
+	else if (e.pageX <= lastX && e.pageY >= lastY) {
+		for (var i = 0; i < snowArray.length; i++) {
+			snowArray[i].x = snowArray[i].x+(e.pageX - lastX);
+			snowArray[i].y = snowArray[i].y-(lastY - e.pageY);
+		}
+	}
+	lastX = e.pageX;
+	lastY = e.pageY;
+};
