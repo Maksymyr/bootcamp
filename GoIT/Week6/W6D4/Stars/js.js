2271e8e90f98b069fdffcdd001e31179cc38cd36
Maksymyr <maksymyr@gmail.com>
2017-10-20 23:57:18 +0300

    stars 2.0

--- GoIT/Week6/W6D4/Stars/js.js
+++ GoIT/Week6/W6D4/Stars/js.js
@@ -1,13 +1,11 @@
 let canvas = document.getElementById('canvas');
 let ctx=canvas.getContext('2d');
-//size  canvas
 window.onresize=size;
 function size(){
 	w = window.innerWidth;
 	h = window.innerHeight;
 	canvas.width=w-6;
 	canvas.height=h-6 ;
-// console.dir(canvas)
 }
 size();
 
@@ -26,20 +24,27 @@ window.requestAnimFrame = (function(){
 function drawIt() {
 	window.requestAnimationFrame(drawIt);
 	ctx.clearRect(0,0,canvas.width,canvas.height);
-	for (var i = 0; i < snowArray.length; i ++){
-		snowArray[i].moving();
+	for (var i = 0; i < starArray.length; i ++){
+		starArray[i].moving();
 	}
 }
 
-class Snowball {
+
+
+class Star {
 	constructor (x, y) {
 		this.x = x;
-		// this.y = 0;
 		this.y = y;
 		this.radius = Math.floor(Math.random()*3+1);
+		// this.opacity = [0.7, 1,1,1,0.7,0.7,1,1,1,1,1,1,1,0.2,1,1,1,1,1,1,1,1,1];
 	}
 	draw() {
 		ctx.beginPath();
+		console.log(this);
+		// ctx.globalAlpha=this.opacity[Math.floor(Math.random()*(this.opacity.length+1))];
+		
+		ctx.fillStyle = "white";
+		
 		ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
 		ctx.fill();
 		ctx.stroke();
@@ -64,20 +69,20 @@ class Snowball {
 		}
 	}
 }
-var snowArray = [];
+var starArray = [];
 
-for (var n = 0; n < 300; n++) {
+for (var n = 0; n < 100; n++) {
+	starArray.push(new Star(Math.floor(Math.random() * w), Math.floor(Math.random() * h)));
+}
+for (var m = 0; m < 100; m++) {
 	setTimeout(function () {
-		snowArray.push(new Snowball(Math.floor(Math.random() * w), Math.floor(Math.random() * h)));
-		console.log(snowArray);
-	}, 10*n);
+	starArray.push(new Star(Math.floor(Math.random() * w), Math.floor(Math.random() * h)));
+	}, 10*m);
 }
 
-
-ctx.fillStyle = "white";
 window.requestAnimationFrame(drawIt);
 
-var checkbox = true;
+// var checkbox = true;
 
 var lastX = 0;
 var lastY = 0;
@@ -89,55 +94,56 @@ document.onmousemove = Dragging;
 
 
 function Dragging (e) {
+	clearInterval(interval);
 	// if(checkbox) {
 	// 	checkbox = false;
 	// document.onmousemove = null;
-		var changeX = (e.pageX - lastX)/10;
-		var changeY = (e.pageY - lastY)/10;
-		// console.log(e.pageX + " : " + e.pageY);
+		var changeX = (e.pageX - lastX)/60;
+		var changeY = (e.pageY - lastY)/60;
+		console.log(e.pageX + " : " + e.pageY);
 	var interval = setInterval(function () {
 		
 		if (e.pageX <= lastX && e.pageY <= lastY) {
-			for (var i = 0; i < snowArray.length; i++) {
-				console.log(i);
-				snowArray[i].x = snowArray[i].x + changeX;
-				snowArray[i].y = snowArray[i].y + changeY;
+			for (var i = 0; i < starArray.length; i++) {
+				// console.log(i);
+				starArray[i].x = starArray[i].x + changeX;
+				starArray[i].y = starArray[i].y + changeY;
 			}
 		}
 		else if (e.pageX >= lastX && e.pageY <= lastY) {
-			for (var i = 0; i < snowArray.length; i++) {
-				snowArray[i].x = snowArray[i].x + changeX;
-				snowArray[i].y = snowArray[i].y + changeY;
+			for (var i = 0; i < starArray.length; i++) {
+				starArray[i].x = starArray[i].x + changeX;
+				starArray[i].y = starArray[i].y + changeY;
 			}
 		}
 		else if (e.pageX >= lastX && e.pageY >= lastY) {
-			for (var i = 0; i < snowArray.length; i++) {
-				snowArray[i].x = snowArray[i].x + changeX;
-				snowArray[i].y = snowArray[i].y + changeY;
+			for (var i = 0; i < starArray.length; i++) {
+				starArray[i].x = starArray[i].x + changeX;
+				starArray[i].y = starArray[i].y + changeY;
 			}
 		}
 		else if (e.pageX <= lastX && e.pageY >= lastY) {
-			for (var i = 0; i < snowArray.length; i++) {
-				snowArray[i].x = snowArray[i].x + changeX;
-				snowArray[i].y = snowArray[i].y + changeY;
+			for (var i = 0; i < starArray.length; i++) {
+				starArray[i].x = starArray[i].x + changeX;
+				starArray[i].y = starArray[i].y + changeY;
 			}
 		}
-		},100);
+		},10);
 
 	// }
 	setTimeout(function () {
 		clearInterval(interval);
 		// checkbox=true;
 		// document.onmousemove = Dragging;
-	}, 1000);
+	}, 1200);
 	lastX = e.pageX;
 	lastY = e.pageY;
 };
-setInterval(function () {
-		snowArray.splice(Math.floor(Math.random()*10),3);
-		console.log(snowArray);
-}, 300);
-setInterval(function () {
-	snowArray.push(new Snowball(Math.floor(Math.random() * w), Math.floor(Math.random() * h)));
-	console.log(snowArray);
-}, 100);
+// setInterval(function () {
+// 		starArray.splice(Math.floor(Math.random()*95),3);
+// 		// console.log(starArray);
+// }, 300);
+// setInterval(function () {
+// 	starArray.push(new Star(Math.floor(Math.random() * w), Math.floor(Math.random() * h)));
+// 	// console.log(starArray);
+// }, 100);
